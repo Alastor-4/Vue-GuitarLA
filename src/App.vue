946a60e6d95846678b373de42a1cae27997f75d0
alastor- <alexismhgdotanime@gmail.com>
2024-05-20 16:04:33 -0400

    project finished

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,92 @@
 <script setup>
-import { onMounted, ref } from 'vue';
-import { db } from './data/guitars.js';
-import Guitar from './components/Guitar.vue';
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
+import { onMounted, ref, watch } from "vue";
+import { db } from "./data/guitars.js";
+import Guitar from "./components/Guitar.vue";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 
 const guitars = ref([]);
 const cart = ref([]);
+const guitar = ref({});
+
+watch(
+  cart,
+  () => {
+    setStorageCart();
+  },
+  { deep: true }
+);
 
 onMounted(() => {
   guitars.value = db;
-})
+  guitar.value = db[3];
+
+  const storageCart = localStorage.getItem("cart");
+  if (storageCart) {
+    cart.value = JSON.parse(storageCart);
+  }
+});
+
+const setStorageCart = () => {
+  localStorage.setItem("cart", JSON.stringify(cart.value));
+};
 
 const addToCart = (guitar) => {
-  const alreadyExist = cart.value.findIndex(product => product.id === guitar.id);
+  const alreadyExist = cart.value.findIndex(
+    (product) => product.id === guitar.id
+  );
+
   if (alreadyExist >= 0) {
     cart.value[alreadyExist].quantity++;
   } else {
     guitar.quantity = 1;
     cart.value.push(guitar);
   }
-}
+};
 
+const increaseQuantity = (id) => {
+  const index = cart.value.findIndex((product) => product.id === id);
+  if (cart.value[index].quantity >= 5) return;
+  cart.value[index].quantity++;
+};
+
+const decreaseQuantity = (id) => {
+  const index = cart.value.findIndex((product) => product.id === id);
+  if (cart.value[index].quantity <= 1) return;
+  cart.value[index].quantity--;
+};
+
+const deleteProduct = (id) => {
+  cart.value = cart.value.filter((product) => product.id !== id);
+};
+
+const deleteAllProducts = () => {
+  cart.value = [];
+};
 </script>
 
 <template>
-  <Header :cart="cart" />
+  <Header
+    :cart="cart"
+    :guitar="guitar"
+    @addToCart="addToCart"
+    @increaseQuantity="increaseQuantity"
+    @decreaseQuantity="decreaseQuantity"
+    @deleteProduct="deleteProduct"
+    @deleteAllProducts="deleteAllProducts"
+  />
 
   <main class="container-xl mt-5">
     <h2 class="text-center">Our Collection</h2>
 
     <div class="row mt-5">
-      <Guitar v-for="guitar in guitars" :guitar="guitar" @click="addToCart(guitar)" />
+      <Guitar
+        v-for="guitar in guitars"
+        :guitar="guitar"
+        @click="addToCart(guitar)"
+      />
     </div>
   </main>
 
   <Footer />
-
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,89 +1,141 @@
 <script setup>
+import { computed } from "vue";
+
 const props = defineProps({
-    cart: {
-        type: Array,
-        required: true,
-    },
+  cart: {
+    type: Array,
+    required: true,
+  },
+  guitar: {
+    type: Object,
+    required: true,
+  },
 });
 
+const totalPrice = computed(() => {
+  return props.cart.reduce(
+    (total, product) => total + product.quantity * product.price,
+    0
+  );
+});
+
+defineEmits([
+  "addToCart",
+  "increaseQuantity",
+  "decreaseQuantity",
+  "deleteProduct",
+  "deleteAllProducts",
+]);
 </script>
 
 <template>
-    <header class="py-5 header">
-        <div class="container-xl">
-            <div class="row justify-content-center justify-content-md-between">
-                <div class="col-8 col-md-3">
-                    <a href="index.html">
-                        <img class="img-fluid" src="/img/logo.svg" alt="imagen logo" />
-                    </a>
-                </div>
-                <nav class="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
-                    <div class="cart">
-                        <img class="img-fluid" src="/img/cart.png" alt="Cart" />
-
-                        <div id="cart" class="bg-white p-3">
-                            <p v-if="cart.length === 0" class="text-center m-0">
-                                The shopping cart is empty
-                            </p>
-                            <div v-else>
-                                <table class="w-100 table">
-                                    <thead>
-                                        <tr>
-                                            <th>Image</th>
-                                            <th>Name</th>
-                                            <th>Price</th>
-                                            <th>Quantity</th>
-                                            <th></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr v-for="guitar in cart">
-                                            <td>
-                                                <img class="img-fluid" :alt="guitar.name"
-                                                    :src="'/img/' + guitar.image + '.jpg'" />
-                                            </td>
-                                            <td>{{ guitar.name }}</td>
-                                            <td class="fw-bold">${{ guitar.price }}</td>
-                                            <td class="flex align-items-start gap-4">
-                                                <button type="button" class="btn btn-dark">-</button>
-                                                {{ guitar.quantity }}
-                                                <button type="button" class="btn btn-dark">+</button>
-                                            </td>
-                                            <td>
-                                                <button class="btn btn-danger" type="button">X</button>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-
-                                <p class="text-end">
-                                    Totalprice: <span class="fw-bold">$899</span>
-                                </p>
-                                <button class="btn btn-dark w-100 mt-3 p-2">Clear cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-            </div>
-            <!--.row-->
-
-            <div class="row mt-5">
-                <div class="col-md-6 text-center text-md-start pt-5">
-                    <h1 class="display-2 fw-bold">Modelo VAI</h1>
-                    <p class="mt-5 fs-5 text-white">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus,
-                        possimus quibusdam dolor nemo velit quo, fuga omnis, iure molestias
-                        optio tempore sint at ipsa dolorum odio exercitationem eos inventore
-                        odit.
-                    </p>
-                    <p class="text-primary fs-1 fw-black">$399</p>
-                    <button type="button" class="btn fs-4 bg-primary text-white py-2 px-5">
-                        Add to the cart
-                    </button>
-                </div>
-            </div>
+  <header class="py-5 header">
+    <div class="container-xl">
+      <div class="row justify-content-center justify-content-md-between">
+        <div class="col-8 col-md-3">
+          <a href="index.html">
+            <img class="img-fluid" src="/img/logo.svg" alt="imagen logo" />
+          </a>
         </div>
+        <nav
+          class="col-md-6 a mt-5 d-flex align-items-start justify-content-end"
+        >
+          <div class="cart">
+            <img class="img-fluid" src="/img/cart.png" alt="Cart" />
 
-        <img class="header-guitar" src="/img/header_guitar.png" alt="imagen header" />
-    </header>
+            <div id="cart" class="bg-white p-3">
+              <p v-if="cart.length === 0" class="text-center m-0">
+                The shopping cart is empty
+              </p>
+              <template v-else>
+                <table class="w-100 table">
+                  <thead>
+                    <tr>
+                      <th>Image</th>
+                      <th>Name</th>
+                      <th>Price</th>
+                      <th>Quantity</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="guitar in cart">
+                      <td>
+                        <img
+                          class="img-fluid"
+                          :alt="guitar.name"
+                          :src="'/img/' + guitar.image + '.jpg'"
+                        />
+                      </td>
+                      <td>{{ guitar.name }}</td>
+                      <td class="fw-bold">${{ guitar.price }}</td>
+                      <td class="flex align-items-start gap-4">
+                        <button
+                          type="button"
+                          class="btn btn-dark"
+                          @click="$emit('decreaseQuantity', guitar.id)"
+                        >
+                          -
+                        </button>
+                        {{ guitar.quantity }}
+                        <button
+                          type="button"
+                          class="btn btn-dark"
+                          @click="$emit('increaseQuantity', guitar.id)"
+                        >
+                          +
+                        </button>
+                      </td>
+                      <td>
+                        <button
+                          class="btn btn-danger"
+                          type="button"
+                          @click="$emit('deleteProduct', guitar.id)"
+                        >
+                          X
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+
+                <p class="text-end">
+                  Total price: <span class="fw-bold">${{ totalPrice }}</span>
+                </p>
+                <button
+                  class="btn btn-dark w-100 mt-3 p-2"
+                  @click="$emit('deleteAllProducts')"
+                >
+                  Clear cart
+                </button>
+              </template>
+            </div>
+          </div>
+        </nav>
+      </div>
+
+      <div class="row mt-5">
+        <div class="col-md-6 text-center text-md-start pt-5">
+          <h1 class="display-2 fw-bold">Model {{ guitar.name }}</h1>
+          <p class="mt-5 fs-5 text-white">
+            {{ guitar.description }}
+          </p>
+          <p class="text-primary fs-1 fw-black">${{ guitar.price }}</p>
+          <button
+            @click="$emit('addToCart', guitar)"
+            type="button"
+            class="btn fs-4 bg-primary text-white py-2 px-5"
+          >
+            Add to the cart
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <img
+      class="header-guitar"
+      src="/img/header_guitar.png"
+      alt="imagen header"
+    />
+  </header>
 </template>
